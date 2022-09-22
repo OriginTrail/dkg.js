63bfc940d353353d8dc61c41775d6f4184b5f0f2
vterzic <veljkot@gmail.com>
2022-09-22 11:40:51 +0200

    Add afterHook option to asset.create method

---
+++ util/empty-hooks.js
@@ -0,0 +1,8 @@
+/**
+ * Empty hooks are used as a fallback hooks
+ * in case user doesn't provide his own implementation
+ * @type {{emptyHooks: exports.emptyHooks}}
+ */
+module.exports = {
+    afterHook: () => {}
+}
