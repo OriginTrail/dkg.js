ffb576000cfb6442194ea4f9863d66dd8bf338b0
zeroxbt <original.swazi@protonmail.com>
2023-02-15 14:30:36 +0100

    add validation for all input arguments

--- services/validation-service.js
+++ services/validation-service.js
@@ -1,8 +1,38 @@
-const { MAX_FILE_SIZE, BLOCKCHAINS, OPERATIONS, GET_OUTPUT_FORMATS } = require('../constants.js');
+const { MAX_FILE_SIZE, OPERATIONS, GET_OUTPUT_FORMATS } = require('../constants.js');
 const { nodeSupported } = require('./utilities.js');
 
 class ValidationService {
-    validatePublishRequest(content, blockchain) {
+    validateRequiredParam(paramName, param) {
+        if (param == null) throw Error(`${paramName} is missing.`);
+    }
+
+    validateParamType(paramName, param, type) {
+        let parameter = param;
+        if (type === 'number') {
+            parameter = parseInt(param, 10);
+        }
+        // eslint-disable-next-line valid-typeof
+        if (typeof parameter !== type) throw Error(`${paramName} must be of type ${type}.`);
+    }
+
+    validateUAL(ual) {
+        this.validateRequiredParam('UAL', ual);
+        this.validateParamType('UAL', ual, 'string');
+
+        const segments = ual.split(':');
+        const argsString = segments.length === 3 ? segments[2] : segments[2] + segments[3];
+        const args = argsString.split('/');
+
+        if (!(args?.length === 3)) throw Error('Invalid UAL.');
+    }
+
+    validateContentType(obj) {
+        if (!(!!obj && typeof obj === 'object')) throw Error('Content must be an object');
+    }
+
+    validateContent(content) {
+        this.validateRequiredParam('content', content);
+
         const keys = Object.keys(content);
 
         if (
@@ -15,77 +45,92 @@ class ValidationService {
             throw Error('Public or private content must be defined');
         }
 
-        if (content.public) {
-            this.validateSize(content.public);
-        }
-        if (content.private) {
-            this.validateSize(content.private);
-        }
-        this.validateBlockchain(blockchain);
-    }
-
-    validateGetRequest(UAL, blockchain, options) {
-        if (!UAL) throw Error('UAL is missing.');
-        this.validateBlockchain(blockchain)
-        this.validateValidate(options?.validate);
-        this.validateOutputFormat(options?.outputFormat);
-    }
-
-    validateAssetTransferRequest(UAL, newOwner, blockchain) {
-        if (!UAL || !newOwner) throw Error('Wrong parameters for the transfer.');
-        this.validateBlockchain(blockchain);
-    }
-
-    validateGetOwnerRequest(UAL, blockchain) {
-        if (!UAL) throw Error('UAL is missing.');
-        this.validateBlockchain(blockchain);
-    }
-
-    validateContentType(obj) {
-        if (!(!!obj && typeof obj === 'object')) throw Error('Content must be an object');
-    }
-
-    validateSize(content) {
-        if (!content) throw Error('No content provided');
         if (Buffer.byteLength(JSON.stringify(content), 'utf-8') > MAX_FILE_SIZE)
             throw Error(`File size limit is ${MAX_FILE_SIZE / (1024 * 1024)}MB.`);
     }
 
+    validateEndpoint(endpoint) {
+        this.validateRequiredParam('endpoint', endpoint);
+        this.validateParamType('endpoint', endpoint, 'string');
+        if (!endpoint.startsWith('http') && !endpoint.startsWith('ws'))
+            throw Error('Endpoint should start with either "http" or "ws"');
+    }
+
+    validatePort(port) {
+        this.validateRequiredParam('port', port);
+        this.validateParamType('port', port, 'number');
+    }
+
+    validateMaxNumberOfRetries(maxNumberOfRetries) {
+        this.validateRequiredParam('maxNumberOfRetries', maxNumberOfRetries);
+        this.validateParamType('maxNumberOfRetries', maxNumberOfRetries, 'number');
+    }
+
+    validateFrequency(frequency) {
+        this.validateRequiredParam('frequency', frequency);
+        this.validateParamType('frequency', frequency, 'number');
+    }
+
+    validateEpochsNum(epochsNum) {
+        this.validateRequiredParam('epochsNum', epochsNum);
+        this.validateParamType('epochsNum', epochsNum, 'number');
+    }
+
+    validateHashFunctionId(hashFunctionId) {
+        this.validateRequiredParam('hashFunctionId', hashFunctionId);
+        this.validateParamType('hashFunctionId', hashFunctionId, 'number');
+    }
+
+    validateScoreFunctionId(scoreFunctionId) {
+        this.validateRequiredParam('scoreFunctionId', scoreFunctionId);
+        this.validateParamType('scoreFunctionId', scoreFunctionId, 'number');
+    }
+
+    validateImmutable(immutable) {
+        this.validateRequiredParam('immutable', immutable);
+        this.validateParamType('immutable', immutable, 'boolean');
+    }
+
+    validateTokenAmount(tokenAmount) {
+        if (tokenAmount == null) return;
+
+        this.validateParamType('tokenAmount', tokenAmount, 'number');
+    }
+
+    validateAuthToken(authToken) {
+        if (authToken == null) return;
+
+        this.validateParamType('authToken', authToken, 'string');
+    }
+
     validateValidate(validate) {
-        if (validate && typeof validate !== 'boolean') throw Error('Valid option must be boolean');
+        this.validateRequiredParam('validate', validate);
+        this.validateParamType('validate', validate, 'boolean');
     }
 
     validateOutputFormat(outputFormat) {
-        if (
-            outputFormat &&
-            GET_OUTPUT_FORMATS.JSON_LD !== outputFormat &&
-            GET_OUTPUT_FORMATS.N_QUADS !== outputFormat
-        )
+        this.validateRequiredParam('outputFormat', outputFormat);
+        if (!Object.values(GET_OUTPUT_FORMATS).includes(outputFormat))
             throw Error('Unknown output format');
     }
 
     validateBlockchain(blockchain, operation) {
-        if (!blockchain) throw Error('Blockchain configuration missing');
-        if (!blockchain.name) throw Error('Blockchain name missing');
-        if (nodeSupported() && !blockchain.rpc && !BLOCKCHAINS[blockchain.name].rpc)
-            throw Error('Blockchain rpc missing');
-        if (!blockchain.hubContract && !BLOCKCHAINS[blockchain.name].hubContract)
-            throw Error('Blockchain hub contract missing');
-        if (operation !== OPERATIONS.GET) {
-            if (!blockchain.publicKey && nodeSupported())
-                throw Error('Blockchain public key missing');
-            if (!blockchain.privateKey && nodeSupported())
-                throw Error('Blockchain private key missing');
+        this.validateRequiredParam('blockchain', blockchain);
+        this.validateRequiredParam('blockchain name', blockchain.name);
+        this.validateRequiredParam('blockchain hub contract', blockchain.hubContract);
+        if (nodeSupported()) {
+            this.validateRequiredParam('blockchain rpc', blockchain.rpc);
+
+            if (operation !== OPERATIONS.GET) {
+                this.validateRequiredParam('blockchain public key', blockchain.publicKey);
+                this.validateRequiredParam('blockchain private key', blockchain.privateKey);
+            }
         }
     }
 
-    validateKeywords(keywords) {
-        if (!keywords) throw Error('No keywords provided');
-        if (!Array.isArray(keywords)) throw Error('Keywords must be an array');
-        if (keywords.length <= 0) throw Error('Keywords array must be non empty');
-        if (keywords.length > 10) throw Error('Too many keywords provided, limit is 10.');
-        if (!keywords.every((i) => typeof i === 'string' && i !== ''))
-            throw Error('all keywords must be non empty strings');
+    validateNewOwner(newOwner) {
+        this.validateRequiredParam('newOwner', newOwner);
+        this.validateParamType('newOwner', newOwner, 'string');
     }
 }
 module.exports = ValidationService;
