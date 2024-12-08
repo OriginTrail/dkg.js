3721ac28e44e3fb233d7680320344677729784c2
brkagithub <mbrkic16@gmail.com>
2024-12-08 17:41:13 +0100

    Dont throw error without a provided node api version

--- services/node-api-service/implementations/http-service.js
+++ services/node-api-service/implementations/http-service.js
@@ -7,16 +7,14 @@ class HttpService {
         this.config = config;
 
         if (
-            !(
-                config.nodeApiVersion === '/' ||
-                config.nodeApiVersion === '/latest' ||
-                /^\/v\d+$/.test(config.nodeApiVersion)
-            )
+            config.nodeApiVersion === '/' ||
+            config.nodeApiVersion === '/latest' ||
+            /^\/v\d+$/.test(config.nodeApiVersion)
         ) {
-            throw Error(`Version must be '/latest', '/' or in '/v{digits}' format.`);
+            this.apiVersion = config.nodeApiVersion;
+        } else {
+            this.apiVersion = '/v0';
         }
-
-        this.apiVersion = config.nodeApiVersion ?? '/v0';
     }
 
     async info(endpoint, port, authToken) {
