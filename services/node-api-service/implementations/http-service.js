f87a70b6bb2f0069eea3e1f4fb4d52d895a0710d
IlijaMar <ilijamarkovic91@gmail.com>
2024-12-17 15:00:00 +0100

    Changed default from v0 to v1

--- services/node-api-service/implementations/http-service.js
+++ services/node-api-service/implementations/http-service.js
@@ -13,7 +13,7 @@ export default class HttpService {
         ) {
             this.apiVersion = config.nodeApiVersion;
         } else {
-            this.apiVersion = '/v0';
+            this.apiVersion = '/v1';
         }
     }
 
