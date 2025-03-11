c698ef8e4ed8a9049861dd13257a66b7fbc6eb07
Zvonimir <zvonimir@Zvonimirs-MacBook-Pro.local>
2025-03-11 10:54:43 +0100

    fix demo

--- examples/demo.js
+++ examples/demo.js
@@ -15,7 +15,7 @@ const DkgClient = new DKG({
     blockchain: {
         name: BLOCKCHAIN_IDS.HARDHAT_1,
         publicKey: PUBLIC_KEY,
-        privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+        privateKey: process.env.PRIVATE_KEY,
     },
     maxNumberOfRetries: 300,
     frequency: 2,
