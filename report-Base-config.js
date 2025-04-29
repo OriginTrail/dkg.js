9fe7adeb395d38f1f6010f0745d977107b2d7730
Bojan <bojan.colic131@gmail.com>
2025-04-29 11:45:33 +0200

    Update reports

--- report-Base-config.js
+++ report-Base-config.js
@@ -29,7 +29,7 @@ const config = defineConfig({
                 links: [
                   {
                     text: 'Testnet Base HTML Report',
-                    url: 'https://titan.dplcenter.xyz/view/Tests/job/Publish%20and%20Query%20Knowledge%20Asset/Base_20Testnet_20Report/*zip*/Base_20Testnet_20Report.zip',
+                    url: 'https://titan.dplcenter.xyz/view/Tests/job/Publish-Query-Get-Knowledge-Asset/Base_20Testnet_20Report/*zip*/Base_20Testnet_20Report.zip',
                   },
                 ],
               },
