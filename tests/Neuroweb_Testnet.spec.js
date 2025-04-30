3aa14836249afedac29bc4790eed1cad89980c14
Bojan <bojan.colic131@gmail.com>
2025-04-30 09:19:50 +0200

    Update tests

--- tests/Neuroweb_Testnet.spec.js
+++ tests/Neuroweb_Testnet.spec.js
@@ -62,6 +62,7 @@ function getRandomDescription() {
           description: getRandomDescription(),
         },
       };
+      await new Promise(resolve => setTimeout(resolve, 5000));
 
       // 1. Publish
       const create_result = await DkgClient.asset.create(content, {
@@ -79,7 +80,8 @@ function getRandomDescription() {
         throw new Error(`Knowledge Asset not published or finalized successfully on ${name}`);
       }
 
-      console.log(`Knowledge Asset Published successfully on ${name}:`, JSON.stringify(create_result, null, 2));
+      console.log(`Knowledge Asset Published successfully on ${name}:`);
+      //console.log(JSON.stringify(create_result, null, 2));
 
       // Save UAL from publish result
       const ual = create_result?.UAL;
@@ -99,12 +101,14 @@ function getRandomDescription() {
       );
 
       assert.ok(queryOperationResult?.data?.length > 0, `Query returned no results for ${name}`);
-      console.log(`Query results from ${name}:`, queryOperationResult);
+      console.log(`Successfully queried Knowledge Asset on ${name}`);
+      //console.log(`Query results from ${name}:`, queryOperationResult);
 
       // 3. Get
       const getResult = await DkgClient.asset.get(ual);
       assert.ok(getResult?.assertion, `Get operation failed or no assertion found for ${name}`);
-      console.log(`Get results from ${name}:`, getResult);
+      console.log(`Successfully got Knowledge Asset on ${name}`);
+      //console.log(`Get results from ${name}:`, getResult);
     });
   });
 });
