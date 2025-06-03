0a8f65787bf4b6511775564ba31256331ea36382
Bojan <bojan.colic131@gmail.com>
2025-06-03 17:21:01 +0200

    Update nodes

--- tests/testnet/Gnosis_Testnet.spec.js
+++ tests/testnet/Gnosis_Testnet.spec.js
@@ -10,8 +10,8 @@ const PRIVATE_KEY = process.env.TESTNET_PRIVATE_KEY;
 
 const nodes = [
   { name: 'Node 01', hostname: 'https://v6-pegasus-node-01.origin-trail.network' },
+  { name: 'Node 04', hostname: 'https://v6-pegasus-node-04.origin-trail.network' },
   { name: 'Node 08', hostname: 'https://v6-pegasus-node-08.origin-trail.network' },
-  { name: 'Node 09', hostname: 'https://v6-pegasus-node-09.origin-trail.network' },
 ];
 
 function getRandomWord() {
