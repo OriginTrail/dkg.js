8484be0bba6e5ea8626cf1745dcf19dc5e31db18
Bojan <bojan.colic131@gmail.com>
2025-06-05 17:02:02 +0200

    Update timeout

--- tests/testnet/Gnosis_Testnet.spec.js
+++ tests/testnet/Gnosis_Testnet.spec.js
@@ -154,7 +154,7 @@ describe('DKG Asset Lifecycle on Gnosis Testnet', function () {
               totalPassed++;
             })(),
             new Promise((_, reject) =>
-              setTimeout(() => reject(new Error(`Timeout after 2 minutes during "${step}" on ${stepNodeName}`)), 2 * 60 * 1000)
+              setTimeout(() => reject(new Error(`Timeout after 2 minutes during "${step}" on ${stepNodeName}`)), 5 * 60 * 1000)
             ),
           ]);
         } catch (error) {
