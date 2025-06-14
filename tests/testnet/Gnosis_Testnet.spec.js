c60dba8b427741df0e09bda11d3f28fbc1d67aa7
Bojan <154557712+Bojan131@users.noreply.github.com>
2025-06-14 12:53:55 +0200

    Update Gnosis_Testnet.spec.js

--- tests/testnet/Gnosis_Testnet.spec.js
+++ tests/testnet/Gnosis_Testnet.spec.js
@@ -353,7 +353,7 @@ describe('DKG Asset Lifecycle on Gnosis Testnet', function () {
       };
 
       const summary = {
-        blockchain_name: BLOCKCHAIN_IDS.NEUROWEB_TESTNET,
+        blockchain_name: DkgClient.config.blockchain.name,
         node_name: name,
         publish_success_rate: safeRate(publishSuccess, publishFail),
         query_success_rate: safeRate(querySuccess, queryFail),
