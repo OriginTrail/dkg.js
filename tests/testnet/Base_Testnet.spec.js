9162902fee35b20bd0aa071ca9d8220aab43f8f1
Bojan <154557712+Bojan131@users.noreply.github.com>
2025-06-14 13:15:46 +0200

    Update Base_Testnet.spec.js

--- tests/testnet/Base_Testnet.spec.js
+++ tests/testnet/Base_Testnet.spec.js
@@ -353,7 +353,7 @@ describe('DKG Asset Lifecycle on Base Testnet', function () {
       };
 
       const summary = {
-        blockchain_name: DkgClient.config.blockchain.name,
+        blockchain_name: BLOCKCHAIN_IDS.BASE_TESTNET,
         node_name: name,
         publish_success_rate: safeRate(publishSuccess, publishFail),
         query_success_rate: safeRate(querySuccess, queryFail),
