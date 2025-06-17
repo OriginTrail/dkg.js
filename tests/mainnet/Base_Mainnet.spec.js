0b2cd59b26dc326609efd3200f1469a69917e3ca
Bojan <bojan.colic131@gmail.com>
2025-06-17 11:46:47 +0200

    add automatic merges to main

--- tests/mainnet/Base_Mainnet.spec.js
+++ tests/mainnet/Base_Mainnet.spec.js
@@ -75,7 +75,7 @@ function logError(error, nodeName) {
 }
 
 describe('DKG Asset Lifecycle on Base Mainnet', function () {
-  this.timeout(65 * 60 * 1000);
+  this.timeout(95 * 60 * 1000);
 
   it('should sequentially test selected node(s)', async () => {
     const NODE_TO_TEST = process.env.NODE_TO_TEST;
