c3f2de010589ebbc808c714f98a3bcf53c59d388
Bojan <bojan.colic131@gmail.com>
2025-12-01 21:04:58 +0100

    neuroweb testnet timeout after 10 minutes

--- tests/testnet/Neuroweb_Testnet.spec.js
+++ tests/testnet/Neuroweb_Testnet.spec.js
@@ -265,7 +265,7 @@ describe('DKG Asset Lifecycle on Neuroweb Testnet', function () {
 
             })(),
             new Promise((_, reject) =>
-              setTimeout(() => reject(new Error(`Timeout after 3 minutes during "publishing" on ${stepNodeName}`)), 3 * 60 * 1000)
+              setTimeout(() => reject(new Error(`Timeout after 10 minutes during "publishing" on ${stepNodeName}`)), 10 * 60 * 1000)
             ),
           ]);
         } catch (error) {
@@ -291,7 +291,7 @@ describe('DKG Asset Lifecycle on Neuroweb Testnet', function () {
               'SELECT'
             ),
             new Promise((_, reject) =>
-              setTimeout(() => reject(new Error(`Timeout after 3 minutes during "querying" on ${stepNodeName}`)), 3 * 60 * 1000)
+              setTimeout(() => reject(new Error(`Timeout after 10 minutes during "querying" on ${stepNodeName}`)), 10 * 60 * 1000)
             ),
           ]);
           const queryEnd = Date.now();
@@ -312,7 +312,7 @@ describe('DKG Asset Lifecycle on Neuroweb Testnet', function () {
           const localGetResult = await Promise.race([
             DkgClient.asset.get(ual),
             new Promise((_, reject) =>
-              setTimeout(() => reject(new Error(`Timeout after 3 minutes during "local get" on ${stepNodeName}`)), 3 * 60 * 1000)
+              setTimeout(() => reject(new Error(`Timeout after 10 minutes during "local get" on ${stepNodeName}`)), 10 * 60 * 1000)
             ),
           ]);
           const localGetEnd = Date.now();
@@ -352,7 +352,7 @@ describe('DKG Asset Lifecycle on Neuroweb Testnet', function () {
           const remoteGetResult = await Promise.race([
             RemoteDkgClient.asset.get(ual),
             new Promise((_, reject) =>
-              setTimeout(() => reject(new Error(`Timeout after 3 minutes during "get" on ${stepNodeName}`)), 3 * 60 * 1000)
+              setTimeout(() => reject(new Error(`Timeout after 10 minutes during "get" on ${stepNodeName}`)), 10 * 60 * 1000)
             ),
           ]);
           const remoteGetEnd = Date.now();
