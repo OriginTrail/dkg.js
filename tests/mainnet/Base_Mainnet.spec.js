f3987556f8352edecee629da271fc44b7b1b76bf
Bojan <bojan.colic131@gmail.com>
2025-12-16 19:17:55 +0100

    buffer for mainnet to be 20

--- tests/mainnet/Base_Mainnet.spec.js
+++ tests/mainnet/Base_Mainnet.spec.js
@@ -435,7 +435,7 @@ describe('DKG Asset Lifecycle on Base Mainnet', function () {
               name: BLOCKCHAIN_IDS.BASE_MAINNET,
               publicKey: nodeKeys[remoteNode.name].publicKey,
               privateKey: nodeKeys[remoteNode.name].privateKey,
-              gasPriceBufferPercent: 10,
+              gasPriceBufferPercent: 20,
             },
             maxNumberOfRetries: 300,
             frequency: 2,
--- tests/mainnet/Neuroweb_Mainnet.spec.js
+++ tests/mainnet/Neuroweb_Mainnet.spec.js
@@ -280,7 +280,7 @@ describe('DKG Asset Lifecycle on Neuroweb Mainnet', function () {
           name: BLOCKCHAIN_IDS.NEUROWEB_MAINNET,
           publicKey: nodeKeys[name].publicKey,
           privateKey: nodeKeys[name].privateKey,
-          gasPriceBufferPercent: 10,
+          gasPriceBufferPercent: 20,
         },
         maxNumberOfRetries: 300,
         frequency: 2,
