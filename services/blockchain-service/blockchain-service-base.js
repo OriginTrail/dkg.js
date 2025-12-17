827f3267f69d82308720616d1218580f78a2f9db
Vujkovic <vujkovicmilan96@gmail.com>
2025-12-17 13:52:11 +0100

    80 percentile fallback

--- services/blockchain-service/blockchain-service-base.js
+++ services/blockchain-service/blockchain-service-base.js
@@ -1402,7 +1402,7 @@ export default class BlockchainServiceBase {
         try {
             // eth_feeHistory params: blockCount, newestBlock, rewardPercentiles
             // [50] = median priority fee per block
-            const priorityFeePercentile = blockchain.priorityFeePercentile ?? 50;
+            const priorityFeePercentile = blockchain.priorityFeePercentile ?? 80;
             const feeHistory = await web3Instance.eth.getFeeHistory(blockCount, 'latest', [priorityFeePercentile]);
 
             // Extract median priority fees from each block (reward[blockIndex][percentileIndex])
