b15ff2d95a13f250267783c42bb6af4b153242c9
Bojan <bojan.colic131@gmail.com>
2026-01-27 11:47:14 +0100

    increase gnosis gas price buffer

--- tests/mainnet/Gnosis_Mainnet.spec.js
+++ tests/mainnet/Gnosis_Mainnet.spec.js
@@ -280,7 +280,7 @@ describe('DKG Asset Lifecycle on Gnosis Mainnet', function () {
           name: BLOCKCHAIN_IDS.GNOSIS_MAINNET,
           publicKey: nodeKeys[name].publicKey,
           privateKey: nodeKeys[name].privateKey,
-          gasPriceBufferPercent: 20,
+          gasPriceBufferPercent: 35,
         },
         maxNumberOfRetries: 300,
         frequency: 2,
--- tests/testnet/Gnosis_Testnet.spec.js
+++ tests/testnet/Gnosis_Testnet.spec.js
@@ -314,7 +314,7 @@ describe('DKG Asset Lifecycle on Gnosis Testnet', function () {
           name: BLOCKCHAIN_IDS.GNOSIS_TESTNET,
           publicKey: nodeKeys[name].publicKey,
           privateKey: nodeKeys[name].privateKey,
-          gasPriceBufferPercent: 20,
+          gasPriceBufferPercent: 50,
         },
         maxNumberOfRetries: 300,
         frequency: 2,
